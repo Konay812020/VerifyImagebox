58979de689dfd2309140a40415dd259a4f765959
konay812020 <konay812020@gmail.com>
2022-09-19 09:07:19 +0630

    create object and function

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 const database = [{
         question: "Choose Traffic Light ?",
-        a: "../img/traffic.jpg",
-        b: "../img/airport.jpg",
-        c: "../img/mountain.jpg",
-        d: "../img/ambulance.jpg",
+        a: "./img/img/traffic.jpg",
+        b: "./img/img/airport.jpg",
+        c: "./img/img/mountain.jpg",
+        d: "./img/img/ambulance.jpg",
         correctanswer: "a"
     },
     {
         question: "Choose ambulance ?",
-        a: "../img/airport.jpg",
-        b: "../img/ambulance.jpg",
-        c: "../img/traffic.jpg",
-        d: "../img/mountain.jpg",
+        a: "./img/img/airport.jpg",
+        b: "./img/img/ambulance.jpg",
+        c: "./img/img/traffic.jpg",
+        d: "./img/img/mountain.jpg",
         correctanswer: "b"
     },
     {
         question: "Choose mountain ?",
-        a: "../img/mountain.jpg",
-        b: "../img/traffic.jpg",
-        c: "../img/ambulance.jpg",
-        d: "../img/airport.jpg",
+        a: "./img/img/mountain.jpg",
+        b: "./img/img/traffic.jpg",
+        c: "./img/img/ambulance.jpg",
+        d: "./img/img/airport.jpg",
         correctanswer: "a"
     },
     {
         question: "Choose ariport ?",
-        a: "../img/traffic.jpg",
-        b: "../img/ambulance.jpg",
-        c: "../img/taffic.jpg",
-        d: "../img/airport.jpg",
+        a: "./img/img/traffic.jpg",
+        b: "./img/img/ambulance.jpg",
+        c: "./img/img/taffic.jpg",
+        d: "./img/img/airport.jpg",
         correctanswer: "d"
     },
 ]
-// console.log(database);
+// console.log(database[0].question);
 
 const getcontainer = document.getElementById('container');
-const getquestion = document.querySelector(".question");
+const getquestion = document.getElementById("question");
 const getanswer = document.querySelectorAll(".answer");
 
 const geta_img = document.getElementById('a_img');
@@ -42,3 +42,22 @@ const geta_img = document.getElementById('a_img');
 const getb_img = document.getElementById('b_img');
 const getc_img = document.getElementById('c_img');
 const getd_img = document.getElementById('d_img');
+
+startquestion();
+
+function startquestion() {
+
+    console.log("i a m owrking")
+
+    getquestion.textContent = database[0].question;
+    // console.log(getquestion)
+
+    geta_img.src = database[0].a;
+    console.log(geta_img.src)
+    getb_img.src = database[0].b;
+    getc_img.src = database[0].c;
+    getd_img.src = database[0].d;
+}
+
+
+// 19BT
